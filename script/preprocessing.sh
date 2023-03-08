f1c0dc9a0f795c44d71cdf6c8fef68fc89718c0e
legoodmanner <yk.lego09@gmail.com>
2023-03-09 00:23:12 +0800

    script debug

--- script/preprocessing.sh
+++ script/preprocessing.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-python3 utils/preprocess.py --audio_dir data/audio --segement_audio_dir data/segement_audio --mel_dir data/mel --beat_dir data/token
-
+python3 utils/preprocess.py --audio_dir data/audio --segement_audio_dir data/segement_audio --mel_dir data/mel --beat_dir data/token --cuda 0
 
